9b419d653f53dd1b63546a65e96264ee7fb8713a
Eve Simbulan <125949931+annisterseve@users.noreply.github.com>
2023-04-01 17:31:52 +0800

    Add files via upload

---
+++ do-while.js
@@ -0,0 +1,6 @@
+var y = 1;
+do {
+  document.write(y * 2 +(', '));
+  i++;
+}
+while (i <= 25);
---
+++ for-loop.js
@@ -0,0 +1,12 @@
+var i = 2
+var z = 1 
+
+for(z); z <= 4109; z){
+    if(z <= 4108){
+        document.write(z = ", ");
+}else
+    document.write(z);
+    }
+    z = z + i
+    i = i*2-1
+}
